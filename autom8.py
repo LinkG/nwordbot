5f4a8fb0493d78506d46357b67312d19bb74a724
nrianjan <niri7879@gmail.com>
2021-07-13 11:39:39 +0530

    initial(3)

--- autom8.py
+++ autom8.py
@@ -5,14 +5,14 @@ import os
 from random_word import RandomWords
 import random
 
-# chrome_options = webdriver.ChromeOptions()
-# chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-# chrome_options.add_argument("--headless")
-# chrome_options.add_argument("--disable-dev-shm-usage")
-# chrome_options.add_argument("--no-sandbox")
-# driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
+chrome_options = webdriver.ChromeOptions()
+chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--disable-dev-shm-usage")
+chrome_options.add_argument("--no-sandbox")
+driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
 
-driver = webdriver.Chrome()
+# driver = webdriver.Chrome()
 r = RandomWords()
 
 driver.get('https://discord.com/channels/846425286765314088/853182317911343154')
