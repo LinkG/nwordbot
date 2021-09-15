b7fc4ad43578e3dffc62d1e35df4625d320cf530
nrianjan <niri7879@gmail.com>
2021-09-15 10:24:03 +0530

    first

--- autom8.py
+++ autom8.py
@@ -8,7 +8,7 @@ import dropbox
 import pafy
 import os
 import asyncio
-import urllib.request
+import requests
 import re
 
 class RepeatedTimer(object):
@@ -162,8 +162,8 @@ async def on_message(message):
 
   if message.content.startswith('*play'):
     search_keyword='+'.join(message.content[message.content.index(' ')+1:].split(' '))
-    html = urllib.request.urlopen("https://www.youtube.com/results?search_query=" + search_keyword)
-    video_ids = re.findall(r"watch\?v=(\S{11})", html.read().decode())
+    html = requests.get("https://www.youtube.com/results?search_query=" + search_keyword).text
+    video_ids = re.findall(r"watch\?v=(\S{11})", html)
     link = "https://www.youtube.com/watch?v=" + video_ids[0]
     video = pafy.new(link)
     audio = video.getbestaudio()
